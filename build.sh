ab79e9b180a48de6b6eb17a38978eb4ea49f1ec1
IQ-7 <iq7.dev@gmail.com>
2022-12-11 01:10:14 +0000

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/IQ-7/manifest.git -b xq -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xq -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IQ-7/local_manifest --depth 1 -b xq-whyred .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
