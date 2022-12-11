d1f20bdd9f85bbae03bae686b8808444adb8ff26
IQ7 <iq7.dev@gmail.com>
2022-12-11 09:34:46 +0700

    [skip ci]

--- script/membangun.sh
+++ script/membangun.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
 set -e
-
-# Lingkungan Utama
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
-nama_perangkat=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
+device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
+grep _jasmine_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=jasmine_sprout
+grep _laurel_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=laurel_sprout
+grep _GM8_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=GM8_sprout
+grep _maple_dsds $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=maple_dsds
 command=$(tail $CIRRUS_WORKING_DIR/build.sh -n +$(expr $(grep '# build rom' $CIRRUS_WORKING_DIR/build.sh -n | cut -f1 -d:) - 1)| head -n -1 | grep -v '# end')
-
 cd $WORKDIR/rom/$name_rom
-
 export ALLOW_MISSING_DEPENDENCIES=true
 export PATH="/usr/lib/ccache:$PATH"
 export CCACHE_DIR=$WORKDIR/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export CCACHE_COMPRESS=true
-
 which ccache
 ccache -M 20
 ccache -z
-
-bash -c "$command" || true & sleep 95m
-bash $CIRRUS_WORKING_DIR/script/check_build.sh
+wget https://raw.githubusercontent.com/NFS-Project/cirrus-ci-skrip/master/rom/config -O $CIRRUS_WORKING_DIR/config
+bash -c "$command" || true
+bash -c "$(curl -sL https://raw.githubusercontent.com/NFS-Project/cirrus-ci-skrip/master/rom/check_build.sh)"
