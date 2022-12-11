83bd68f03f96bb8ea95a93dc4621afba54bb26f7
IQ-7 <iq7.dev@gmail.com>
2022-12-11 00:32:55 +0000

    import build_ci [skip ci]

---
+++ build.sh
@@ -0,0 +1,25 @@
+#sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-Project/local_manifest --depth 1 -b rosy-SparkOS-13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source $CIRRUS_WORKING_DIR/script/config
+timeStart
+
+source build/envsetup.sh
+export TZ=Asia/Jakarta
+export KBUILD_BUILD_USER=rosy
+export KBUILD_BUILD_HOST=nfsproject
+export BUILD_USERNAME=rosy
+export BUILD_HOSTNAME=nfsproject
+lunch spark_rosy-userdebug
+mkfifo reading
+tee "${BUILDLOG}" < reading &
+build_message "Building Started"
+progress &
+mka bacon -j8  > reading
+
+retVal=$?
+timeEnd
+statusBuild
