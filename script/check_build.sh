fa84bfbbea509c17fa6cfb53c3b72e448526d30c
IQ7 <iq7.dev@gmail.com>
2022-12-11 09:29:15 +0700

    [skip ci] update

--- script/check_build.sh
+++ script/check_build.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
-pesan() {
+msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
 
-# Lingkungan Utama
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
-nama_perangkat=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
-
-build_gagal=$(grep 'FAILED:' $WORKDIR/rom/$name_rom/build.log -m1 || true)
-if [[ $build_gagal == *'FAILED:'* ]]; then
-    cd $WORKDIR/rom/$name_rom
-    pesan ⛔ Build gagal...
-    curl -F document=@build.log "https://api.telegram.org/bot${TG_TOKEN}/sendDocument" \
-    -F chat_id="${TG_CHAT_ID}" \
-    -F "disable_web_page_preview=true" \
-    -F "parse_mode=html" \
-    -F caption="⛔ ${nama_perangkat} Build $name_rom error..."
-fi
-
-build_berhasil=$(grep '#### build completed successfully' $WORKDIR/rom/$name_rom/build.log -m1 || true)
-if [[ $build_berhasil == *'#### build completed successfully'* ]]; then
-    pesan ✅ Build selesai 100%
+device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
+grep _jasmine_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=jasmine_sprout
+grep _laurel_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=laurel_sprout
+grep _GM8_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=GM8_sprout
+grep _maple_dsds $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=maple_dsds
+a=$(grep '#### build completed successfully' $WORKDIR/rom/$name_rom/build.log -m1 || true)
+if [[ $a == *'#### build completed successfully'* ]]
+  then
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  msg ✅ Build is completed 100% ✅
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
 else
-    pesan ❌ Build belum selesai...
-    echo Build ROM gagal! kami melanjutkan dengan mengunggah ccache...
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  msg ❌ ...Build not completed... ❌
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  echo lanjut upload ccache aja 😅
 fi
