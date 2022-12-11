1d3d5c1e054572e885d634e5078395a2c468a791
IQ7 <iq7.dev@gmail.com>
2022-12-11 09:44:51 +0700

    [skip ci]

--- script/mengemas.sh
+++ script/mengemas.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-pesan() {
+msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
 
-pesan_telegram() {
+telegram_message() {
     curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
     -d chat_id="$TG_CHAT_ID" \
     -d parse_mode="HTML" \
@@ -12,81 +12,100 @@ pesan_telegram() {
 }
 
 function enviroment() {
-    perangkat=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
-    nama_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
-    nama_file=$(cd $WORKDIR/rom/$nama_rom/out/target/product/$perangkat && ls *.zip)
-    brach=$(grep init $CIRRUS_WORKING_DIR/build.sh | awk -F "-b " '{print $2}' | awk '{print $1}')
-    rel_date=$(date "+%Y%m%d")
-    DATE_L=$(date +%d\ %B\ %Y)
-    DATE_S=$(date +"%T")
+device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
+grep _jasmine_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=jasmine_sprout
+grep _laurel_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=laurel_sprout
+grep _GM8_sprout $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=GM8_sprout
+grep _maple_dsds $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=maple_dsds
+name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
+file_name=$(cd $WORKDIR/rom/$name_rom/out/target/product/$device && ls *.zip)
+branch_name=$(grep init $CIRRUS_WORKING_DIR/build.sh | awk -F "-b " '{print $2}' | awk '{print $1}')
+rel_date=$(date "+%Y%m%d")
+DATE_L=$(date +%d\ %B\ %Y)
+DATE_S=$(date +"%T")
 }
 
 function upload_rom() {
-pesan Mengunggah ROM...
-
-cd $WORKDIR/rom/$nama_rom
-
-nama_file=$(basename out/target/product/$perangkat/*.zip)
-tautan=https://file.cloudmobx.workers.dev/$nama_rom/$perangkat/$nama_file
-maintainer=https://t.me/mobxprjkt
-
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip mobx:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-cd $WORKDIR/rom/$nama_rom/out/target/product/$perangkat
-
+echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+msg Upload rom..
+echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+cd $WORKDIR/rom/$name_rom
+engzip=$(ls out/target/product/$device/*-eng*.zip | grep -v "retrofit" || true)
+otazip=$(ls out/target/product/$device/*-ota-*.zip | grep -v "hentai" | grep -v "evolution" || true)
+awaken=$(ls out/target/product/$device/Project-Awaken*.zip || true)
+octavi=$(ls out/target/product/$device/OctaviOS-R*.zip || true)
+p404=$(ls out/target/product/$device/?.*zip || true)
+cipher=$(ls out/target/product/$device/CipherOS-*-OTA-*.zip || true)
+rm -rf $engzip $otazip $awaken $octavi $p404 $cipher
+file_name=$(basename out/target/product/$device/*.zip)
+DL_LINK=$GDRIVE_INDEX/$name_rom/$device/$file_name
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip NFS:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+cd $WORKDIR/rom/$name_rom/out/target/product/$device
 echo -e \
 "
-<b>Building selesai...</b>
-<b>==============================</b>
-<b>ROM :</b> <b>${nama_rom} | ${brach}</b>
-<b>Nama file :</b> <b><code>"${nama_file}"</code></b>
-<b>Lama build : "$(grep "#### build completed successfully" $WORKDIR/rom/$nama_rom/build.log -m 1 | cut -d '(' -f 2)"</b>
-<b>Perangkat : POCO M3 | REDMI 9T</b>
-<b>Size : "$(ls -lh *zip | cut -d ' ' -f5)"</b>
-<b>Download Link :</b> <a href=\"${tautan}\">Here</a>
-<b>Tanggal : "$(date +%d\ %B\ %Y)"</b>
-<b>Zona waktu : "$(date +%T)"</b>
-<b>==============================</b>
-<b>📕 MD5 :</b> <code>"$(md5sum *zip | cut -d' ' -f1)"</code>
-<b>📘 SHA1 :</b> <code>"$(sha1sum *zip | cut -d' ' -f1)"</code>
-<b>==============================</b>
-<b>🌀 Maintainer : <a href=\"${maintainer}\">Yovie</a></b>
+<b>✅ Build Completed Successfully ✅</b>
+━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+<b>🚀 Rom Name :- ${name_rom}</b>
+<b>📁 File Name :-</b> <code>"${file_name}"</code>
+<b>⏰ Timer Build :- "$(grep "#### build completed successfully" $WORKDIR/rom/$name_rom/build.log -m 1 | cut -d '(' -f 2)"</b>
+<b>📱 Device :- "${device}"</b>
+<b>📂 Size :- "$(ls -lh *zip | cut -d ' ' -f5)"</b>
+<b>🖥 Branch Build :- "${branch_name}"</b>
+<b>📥 Download Link :-</b> <a href=\"${DL_LINK}\">Here</a>
+<b>📅 Date :- "$(date +%d\ %B\ %Y)"</b>
+<b>🕔 Time Zone :- "$(date +%T)"</b>
+<b>📕 MD5 :-</b> <code>"$(md5sum *zip | cut -d' ' -f1)"</code>
+<b>📘 SHA1 :-</b> <code>"$(sha1sum *zip | cut -d' ' -f1)"</code>
+━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+<b>👮 By : "$(cd $CIRRUS_WORKING_DIR && git log --pretty=format:'%an' -1)"</b>
 " > tg.html
 TG_TEXT=$(< tg.html)
-pesan_telegram "$TG_TEXT"
-
+telegram_message "$TG_TEXT"
 echo
-pesan Mengunggah ROM berhasil...
+echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+msg Upload rom succes..
+echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
 echo
-echo Tautan Unduhan: ${tautan}
+echo Download Link: ${DL_LINK}
 echo
 echo
 }
 
 function upload_ccache() {
-    cd $WORKDIR
-    compress() {
-        tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
-    }
-    time compress ccache 1
-    rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz mobx:ccache/$perangkat/$nama_rom -P
-    rm -rf ccache.tar.gz
-    pesan Mengunggah ccache berhasil...
+cd $WORKDIR
+com ()
+{
+  tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
+}
+time com ccache 1
+rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz NFS:ccache/$name_rom/$device -P
+rm -rf ccache.tar.gz
+echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+msg Upload ccache succes..
+echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
 }
 
 function upload() {
 enviroment
-sukses=$(grep '#### build completed successfully' $WORKDIR/rom/$nama_rom/build.log -m1 || true)
-if [[ $sukses == *'#### build completed successfully'* ]]; then
-    pesan Build selesai 100% sukses ✅
-    echo
-    upload_rom
-    pesan Upload ccache..
-    upload_ccache
+a=$(grep '#### build completed successfully' $WORKDIR/rom/$name_rom/build.log -m1 || true)
+if [[ $a == *'#### build completed successfully'* ]]
+  then
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  msg ✅ Build completed 100% success ✅
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  echo
+  echo
+  upload_rom
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  msg Upload ccache..
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  upload_ccache
 else
-    pesan Build belum selesai, Unggah ccache saja ❌
-    pesan Upload ccache..
-    upload_ccache
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  msg ❌ Build not completed, Upload ccache only ❌
+  msg Upload ccache..
+  echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+  upload_ccache
 fi
 }
 
