2288026d1f7b0c3d1606b5ee1dba0fff0477b15f
IQ7 <iq7.dev@gmail.com>
2023-01-18 12:40:00 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/IQ-7/manifest.git -b xq -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IQ-7/local_manifest --depth 1 -b xq-whyred .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/IQ-7/local_manifest --depth 1 -b corvus-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,12 +10,12 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
-lunch xtended_whyred-userdebug
+lunch corvus_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make xtended -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+make corvus -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
