6ba06016f3bd869af71250eafd21e5d41150c293
prokkton00 <98280328+prokkton00@users.noreply.github.com>
2023-01-19 08:34:16 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IQ-7/local_manifest --depth 1 -b corvus-12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+git clone --depth=1 https://github.com/parikk/device_xiaomi_lmi.git -b 13-evo device/xiaomi/lmi
+git clone --depth=1 https://github.com/parikk/vendor_xiaomi_lmi.git -b 13-evo vendor/xiaomi/lmi
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
+git clone --depth=1 https://gitlab.com/Roxor-007/WeebX_clang16.git -b main prebuilts/clang/host/linux-x86/clang-weebx
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
-source build/envsetup.sh
-export BUILD_USERNAME=zacky
+. build/envsetup.sh
+export BUILD_USERNAME=parikk
 export BUILD_HOSTNAME=android-build
-lunch corvus_whyred-userdebug
+lunch evolution_lmi-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make corvus -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
+mka evolution -j16  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
