ab017c022f24b5d448a533638262bc79ed6732e0
prokkton00 <parikk@protonmail.com>
2023-01-20 10:38:05 +0000

    yes

--- script/check_build.sh
+++ script/check_build.sh
@@ -6,7 +6,6 @@ msg() {
 
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
-grep _lmi $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=lmi
 a=$(grep '#### build completed successfully' $WORKDIR/rom/$name_rom/build.log -m1 || true)
 if [[ $a == *'#### build completed successfully'* ]]
   then
--- script/membangun.sh
+++ script/membangun.sh
@@ -3,7 +3,6 @@
 set -e
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
-grep _lmi $CIRRUS_WORKING_DIR/build.sh > /dev/null && device=lmi
 command=$(tail $CIRRUS_WORKING_DIR/build.sh -n +$(expr $(grep '# build rom' $CIRRUS_WORKING_DIR/build.sh -n | cut -f1 -d:) - 1)| head -n -1 | grep -v '# end')
 cd $WORKDIR/rom/$name_rom
 export PATH="/usr/lib/ccache:$PATH"
