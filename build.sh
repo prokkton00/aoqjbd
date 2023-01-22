dfe1c72d7bf4d2650bfe2fda5e75234ae74194e2
prokkton00 <98280328+prokkton00@users.noreply.github.com>
2023-01-22 19:24:01 +0700

    ci: evox ccache

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/parikk/device_xiaomi_lmi.git -b 13-evo device/xiaomi/lmi
-git clone --depth=1 https://github.com/parikk/vendor_xiaomi_lmi.git -b 13-evo vendor/xiaomi/lmi
+git clone --depth=1 https://github.com/parikk/device_xiaomi_lmi.git -b 13-evox device/xiaomi/lmi
+git clone --depth=1 https://github.com/parikk/vendor_xiaomi_lmi.git -b 13-evox vendor/xiaomi/lmi
 git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
 git clone --depth=1 https://gitlab.com/Roxor-007/WeebX_clang16.git -b main prebuilts/clang/host/linux-x86/clang-weebx
 
@@ -12,8 +12,9 @@ timeStart
 
 . build/envsetup.sh
 export BUILD_USERNAME=parikk
-export BUILD_HOSTNAME=android-build
-lunch evolution_lmi-userdebug
+export BUILD_HOSTNAME=parikk-build
+export EVO_BUILD_TYPE=OFFICIAL
+lunch evolution_lmi-user
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
