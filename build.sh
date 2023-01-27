d4a905d8d8ee8bcb7e9fb229158a6b09b44d12c7
prokkton00 <98280328+prokkton00@users.noreply.github.com>
2023-01-27 21:10:02 +0700

    ci: aospa

--- build.sh
+++ build.sh
@@ -1,25 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/TheDoctor12/manifest.git -b topaz
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/parikk/device_xiaomi_lmi.git -b 13-evox device/xiaomi/lmi
-git clone --depth=1 https://github.com/parikk/vendor_xiaomi_lmi.git -b 13-evox vendor/xiaomi/lmi
-git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
-git clone --depth=1 https://gitlab.com/Roxor-007/WeebX_clang16.git -b main prebuilts/clang/host/linux-x86/clang-weebx
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
-. build/envsetup.sh
-export BUILD_USERNAME=parikk
-export BUILD_HOSTNAME=parikk-build
-export EVO_BUILD_TYPE=OFFICIAL
-lunch evolution_lmi-user
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka evolution -j16  > reading & sleep 95m # Jangan di hapus text line (> reading)
+./rom-build.sh lmi -t user -z  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
